e29defe3f0f7e06127be4e817a62a5f2dd6d5f5f
Ashflight <tomandyyao72@gmail.com>
2025-07-02 17:43:18 -0700

    finished start screen, main game has a placeholder screen

--- game.py
+++ game.py
@@ -1,5 +1,11 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QLabel, QMainWindow
 
 ## creates game at desired difficulty level to let player play
 def create_game_screen(mainWindow : QMainWindow):
-    pass
+    ## placeholder screen
+    placeholder = QLabel("PLACEHOLDER")
+    mainWindow.setCentralWidget(placeholder)
+
+    ## set up boards first
+    ## creates digging/flagging toggle
+    ## set up display, click display to dig or flag
--- start_screen.py
+++ start_screen.py
@@ -1,5 +1,7 @@
+from game import create_game_screen
+
 from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtWidgets import QLabel, QMainWindow, QPushButton, QVBoxLayout
+from PyQt6.QtWidgets import QLabel, QMainWindow, QPushButton, QVBoxLayout, QWidget
 
 ## explains instructions and lets player select difficulty mode
 def create_start_screen(mainWindow : QMainWindow):
@@ -10,16 +12,36 @@ def create_start_screen(mainWindow : QMainWindow):
     font.setPointSize(50)
     title.setFont(font)
     title.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+    title.setFixedHeight(100)
     layout.addWidget(title)
 
     instructions = QLabel("Pick a difficulty setting.")
+    font2 = instructions.font()
+    font2.setPointSize(15)
+    instructions.setFont(font2)
+    instructions.setFixedHeight(30)
     layout.addWidget(instructions)
 
-    difficulties = ["Easy", "Medium", "Hard"]
-    for i in range(3):
+    difficulties = ["Easy", "Medium", "Hard", "Very Hard"]
+    for i in range(4):
         button = QPushButton(difficulties[i])
-        button.setFixedSize(QSize(100, 50))
-        button.clicked.connect(lambda: difficulty_selected(button, mainWindow))
+        button.setObjectName(difficulties[i])
+        buttonFont = button.font()
+        buttonFont.setPointSize(15)
+        button.setFont(buttonFont)
+        button.setFixedSize(QSize(150, 75))
+        button.clicked.connect(lambda _, b=button: difficulty_selected(b, mainWindow))
+        layout.addWidget(button, alignment=Qt.AlignmentFlag.AlignHCenter)
+
+    widget = QWidget()
+    widget.setLayout(layout)
+    mainWindow.setCentralWidget(widget)
 
 
-def difficulty_selected(button : QPushButton, mainWindow : QMainWindow)
+def difficulty_selected(button : QPushButton, mainWindow : QMainWindow):
+    mineCounts = {"Easy" : 20,
+                  "Medium" : 30,
+                  "Hard" : 40,
+                  "Very Hard" : 50}
+    mainWindow.mineCount = mineCounts[button.objectName()]
+    create_game_screen(mainWindow)
